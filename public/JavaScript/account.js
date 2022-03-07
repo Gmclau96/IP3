dc7bdf6279a54562dec7491e0eee3a3a1d4f36b1
Jordan Hannah <jhanna203@caledonian.ac.uk>
2022-03-07 13:16:36 +0000

    more code added

--- public/JavaScript/account.js
+++ public/JavaScript/account.js
@@ -1,1 +1,16 @@
 // JavaScript file for Account Page //
+
+/* Nav Menu display handling */
+const hamburger = document.getElementById("burger-menu");
+const navContent = document.getElementById("nav-content");
+const navMenu = document.getElementsByClassName("nav-menu");
+
+//Display & Hide Nav Menu
+hamburger.addEventListener("click", () => {
+    if (navContent.style.display !== "none") {
+      navContent.style.display = "none";
+      } else {
+        navContent.style.display = "block";
+        navMenu.style.backgroundColor = "black";
+      }
+    });
